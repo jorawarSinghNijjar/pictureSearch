a527ce48046b4244ddbd20c6b4bc641793403376
Jorawar Singh Nijjar <kawal5_nijjar@yahoo.co.in>
2020-02-17 23:44:45 -0500

    Updates

---
+++ precache-manifest.486313666577e181825c9922018f82bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7373c2eeee5752979108b58e28268567",
+    "url": "/pictureSearch/index.html"
+  },
+  {
+    "revision": "e4526ad2d3d61cf2b740",
+    "url": "/pictureSearch/static/css/main.84a9c081.chunk.css"
+  },
+  {
+    "revision": "722aeff2d0911534f063",
+    "url": "/pictureSearch/static/js/2.52e7e234.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pictureSearch/static/js/2.52e7e234.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4526ad2d3d61cf2b740",
+    "url": "/pictureSearch/static/js/main.4f37eb1b.chunk.js"
+  },
+  {
+    "revision": "2f3477b0ee388a88feef",
+    "url": "/pictureSearch/static/js/runtime-main.d605f2da.js"
+  }
+]);
