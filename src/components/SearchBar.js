6cdb97fc44d28be7e645de7487deeae52493ea89
Jorawar Singh Nijjar <kawal5_nijjar@yahoo.co.in>
2020-06-13 14:36:52 -0400

    improved design

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,15 +18,21 @@ class SearchBar extends React.Component {
                 <form onSubmit={this.onFormSubmit} className="ui form">
                 <label style={{color:"white"}}>Image Search</label>
 
-                    <div className="inline fields">
-                        <div className="fourteen wide field">
-                            
-                            <input type="text" placeholder="Enter any keyword to search images..." value={this.state.term} onChange={(e) => this.setState({term: e.target.value})} />
+                    <div className="two fields">
+                        <div className="thirteen wide field">
+                            <div className="ui left icon input">
+                                <i class="images icon"></i>
+                                <input type="text" placeholder="Enter any keyword to search images..." value={this.state.term} onChange={(e) => this.setState({term: e.target.value})} />
+                            </div>
                         </div> 
-                        <div className="two wide field">
-                            <input type="submit" className="ui blue button" value="Search" />
+                        <div className="three wide field">
+                            <div className="ui left icon input">
+                                <i class="search icon"></i>
+                                <input type="submit" className="ui blue button" value="Search" style={{textAlign:"center"}}/>
+                            </div>
                         </div>
-                    </div>  
+                    </div>
+                    <a class="ui teal right ribbon label" href="https://unsplash.com" >Powered by UNSPLASH</a>
                 </form>
             </div>
         );
